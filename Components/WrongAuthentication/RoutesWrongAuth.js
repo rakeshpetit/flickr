8df1c623352371699ce2fa6e10153bf7f740691e
Rakesh Arunachalam <raar@danskeit.co.in>
2018-06-15 17:22:52 +0530

    navigate to flows

--- Components/WrongAuthentication/RoutesWrongAuth.js
+++ Components/WrongAuthentication/RoutesWrongAuth.js
@@ -1,20 +1,52 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Button, Text } from 'react-native';
+import PropTypes from 'prop-types';
 import { StackNavigator } from 'react-navigation';
 
 const SignIn = (props) => {
-  console.log('props', props);
-  return <Text>Component Signin</Text>;
+  const { navigation } = props;
+  return (
+    <View>
+      <Text>Component Signin</Text>
+      <Button
+        onPress={() => {
+        navigation.navigate('SignUp');
+      }}
+        title="Go back"
+      />
+    </View>
+  );
 };
 
-const SignUp = () => <Text>Component Signup</Text>;
+SignIn.propTypes = {
+  navigation: PropTypes.func.isRequired
+};
+
+const SignUp = (props) => {
+  const { navigation } = props;
+  return (
+    <View>
+      <Text>Component Signup</Text>
+      <Button
+        onPress={() => {
+        navigation.navigate('SignIn');
+      }}
+        title="Sign in"
+      />
+    </View>
+  );
+};
+
+SignUp.propTypes = {
+  navigation: PropTypes.func.isRequired
+};
 
 const RoutesWrongAuth = StackNavigator({
-  SignIn: {
-    screen: SignIn
-  },
   SignUp: {
     screen: SignUp
+  },
+  SignIn: {
+    screen: SignIn
   }
 });
 
