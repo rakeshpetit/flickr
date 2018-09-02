a30cf14039ca0ce83e4f7c7526e3cd643008f378
Rakesh <rakeshpetit@gmail.com>
2018-09-02 12:48:26 +0530

    profile pic in firebase db

--- sagas/sagas.js
+++ sagas/sagas.js
@@ -1,17 +1,19 @@
-import { takeEvery, call, put } from 'redux-saga/effects';
+import { takeEvery, call } from 'redux-saga/effects';
+import axios from 'axios';
 import { authenticate, database } from '../Store/Firebase';
-import { REGISTER, LOGIN, saveImage, SAVE_IMAGE_CLOUD } from '../actions';
+import { REGISTER, LOGIN, SAVE_IMAGE_CLOUD } from '../actions';
 import { ENV as env } from '../const';
 
 const registerUserWithFirebase = values => authenticate
   .createUserWithEmailAndPassword(values.email, values.password)
   .then(success => success);
 
-const registerFirebaseDB = ({ uid, email, fullName }) => {
-  console.log('registerInternalDB', uid, email, fullName);
+const registerFirebaseDB = ({ uid, email, fullName, profilePhotoUrl }) => {
+  console.log('registerInternalDB', uid, email, fullName, profilePhotoUrl);
   database.ref(`users/${uid}`).set({
     fullName,
-    email
+    email,
+    profilePhotoUrl
   });
 };
 
@@ -21,45 +23,52 @@ const loginUserWithFirebase = (values) => {
     .then(success => success);
 };
 
-function* imageUploadCloudinary(image) {
-  console.log('imageUploadCloudinary', image);
+function imageUploadCloudinary(image) {
   const photo = {
     uri: image.uri,
     name: image.name,
     type: image.type
   };
-  console.log('photo', photo);
-  console.log('env', env);
   const formPhoto = new FormData();
   formPhoto.append('upload_preset', env.CLOUDINARY_PRESET);
   formPhoto.append('file', photo);
-  return fetch(env.CLOUDINARY_API, {
-    method: 'POST',
-    body: formPhoto
-  }).then(response => response.json());
-  yield put(saveImage(image));
+  return axios.post(env.CLOUDINARY_API, formPhoto)
+    .then(res => res.data)
+    .catch(error => error);
 }
 
-function* registerUserCall({ data }) {  
+function* registerUserCall({ data }) {
   console.log('values registerUserCall', data);
   try {
-    // const registerData = yield call(registerUserWithFirebase, values.data);
-    // console.log('registerData', registerData);
-    // const { email, uid } = registerData.user;
-    const { fullName } = data;    
     const imageResp = yield call(imageUploadCloudinary, data.image);
-    console.log('imageResp', imageResp.secure_url);
-    // yield call(registerFirebaseDB, { uid, email, fullName });
+    console.log('imageResp', imageResp);
+    if (imageResp !== null && imageResp !== undefined) {
+      const profilePhotoUrl = imageResp.secure_url;
+      console.log('profilePhotoUrl', profilePhotoUrl);
+      const registerData = yield call(registerUserWithFirebase, data);
+      console.log('registerData', registerData);
+      const { email, uid } = registerData.user;
+      const { fullName } = data;
+      yield call(
+        registerFirebaseDB,
+        {
+          uid,
+          email,
+          fullName,
+          profilePhotoUrl
+        }
+      );
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
 function* loginUser(values) {
-  console.log('values loginUser', values);
+  // console.log('values loginUser', values);
   try {
-    const loginData = yield call(loginUserWithFirebase, values.data);
-    console.log('loginData loginUser', loginData);
+    yield call(loginUserWithFirebase, values.data);
+    // console.log('loginData loginUser', loginData);
   } catch (error) {
     console.log(error);
   }
@@ -69,5 +78,4 @@ export default function* funcMain() {
   yield takeEvery(REGISTER, registerUserCall);
   yield takeEvery(LOGIN, loginUser);
   yield takeEvery(SAVE_IMAGE_CLOUD, imageUploadCloudinary);
-  console.log('Generator function1');
 }
